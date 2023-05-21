80ecd47aa01cd50bb6529ba91a09bf29a170cdc2
Karan Ahlawat <ahlawatkaran12@gmail.com>
2023-05-21 18:37:52 +0530

    add a starting README

--- scala-ts-mode.el
+++ scala-ts-mode.el
@@ -1,7 +1,30 @@
-;;; scala-ts-mode.el --- Scala Tree-Sitter Mode -*- lexical-binding: t; -*-
+;;; scala-ts-mode.el --- Scala Tree-Sitter Mode      -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023  Karan Ahlawat
+
+;; Author: Karan Ahlawat(require 'treesit) <ahlawatkaran12@gmail.com>
+;; Keywords: scala, languages, tree-sitter
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 ;;; Commentary:
-;;; Treesitter based Scala major mode for Emacs (based on the new treesit lib)
+
+;; 
+
 ;;; Code:
+
+
 (require 'treesit)
 (eval-when-compile (require 'rx))
 
@@ -228,6 +251,7 @@
     (treesit-parser-create 'scala)
 
     (setq-local treesit-font-lock-settings scala-ts--treesit-font-lock-settings)
+    ;; TODO Split this into levels to respect user choices
     (setq-local treesit-font-lock-feature-list '((comment
                                                   definition
                                                   keyword
