0ef7a9ce171867f41be7a3cb6707ee00cf3d1f86
Karan Ahlawat <ahlawatkaran12@gmail.com>
2023-06-22 15:19:53 +0530

    [add] Some syntactic clean-up

--- scala-ts-mode.el
+++ scala-ts-mode.el
@@ -33,9 +33,9 @@
 
 ;;; Code:
 
-
+(require 'rx)
 (require 'treesit)
-(eval-when-compile (require 'rx))
+(require 'thingatpt)
 
 (declare-function treesit-parser-create "treesit.c")
 (declare-function treesit-node-type "treesit.c")
@@ -51,14 +51,14 @@
 (declare-function treesit-node-end "treesit.c")
 (declare-function treesit-node-child "treesit.c")
 
-(defcustom scala-ts-mode-indent-offset 2
+(defcustom scala-ts-indent-offset 2
   "Number of spaces for each indentation in `scala-ts-mode'."
   :version "29.1"
   :type 'integer
   :safe 'integerp
   :group 'scala-ts)
 
-(defvar scala-ts-mode--syntax-table
+(defvar scala-ts--syntax-table
   (let ((table (make-syntax-table)))
     ;; "." denotes punctuation
     (modify-syntax-entry ?+ "." table)
@@ -75,77 +75,53 @@
     table)
   "Syntax table for `scala-ts-mode'.")
 
-(defvar scala-ts-mode--keywords
-  '("case"
-    "class"
-    "enum"
-    "extends"
-    "derives"
-    "finally"
-    "object"
-    "override"
-    "package"
-    "trait"
-    "type"
-    "val"
-    "var"
-    "with"
-    "given"
-    "using"
-    "end"
-    "implicit"
-    "extension"
-    "with"
-    "def"
-    "import"
-    "export"
-    "new")
+
+;; utility functions -- begin
+
+(defun scala-ts--node-type= (type node)
+  "Compare TYPE and type of NODE for string equality."
+  (string= type (treesit-node-type node)))
+
+;; utility functions -- end
+
+;; Keywords
+(defvar scala-ts--keywords
+  '("case" "class" "enum" "extends" "derives" "finally"
+    "object" "override" "package" "trait" "type" "val"
+    "var" "with" "given" "using" "end" "implicit"
+    "extension" "with" "def" "import" "export" "new")
   "Keywords for `scala-ts-mode'.")
 
-(defvar scala-ts-mode--keywords-type-qualifiers
-  '("abstract"
-    "final"
-    "lazy"
-    "sealed"
-    "private"
-    "protected")
+(defvar scala-ts--keywords-type-qualifiers
+  '("abstract" "final" "lazy"
+    "sealed" "private" "protected")
   "Type qualifiers for `scala-ts-mode'.")
 
-(defvar scala-ts-mode--keywords-control
-  '("if"
-    "then"
-    "else"
-    "match"
-    "do"
-    "for"
-    "yield"
-    "while"
-    "try"
-    "catch"
-    "throw"
-    "return")
+(defvar scala-ts--keywords-control
+  '("if" "then" "else" "match" "do" "for" "yield"
+    "while" "try" "catch" "throw" "return")
   "Control flow for `scala-ts-mode'.")
 
-(defvar scala-ts-mode--brackets
+(defvar scala-ts--brackets
   '("(" ")" "[" "]" "{" "}")
   "Brackets for `scala-ts-mode'.")
 
-(defvar scala-ts-mode--delimiters
-  '("." ",")
+(defvar scala-ts--delimiters
+  '("." "," ";")
   "Delimiters for `scala-ts-mode'.")
 
-(defvar scala-ts-mode--treesit-font-lock-settings
+(defvar scala-ts--treesit-font-lock-settings
   (treesit-font-lock-rules
    :language 'scala
    :feature 'keyword
-   `([,@scala-ts-mode--keywords ] @font-lock-keyword-face
-     [,@scala-ts-mode--keywords-type-qualifiers] @font-lock-keyword-face
+   `([,@scala-ts--keywords ] @font-lock-keyword-face
+     [,@scala-ts--keywords-type-qualifiers] @font-lock-keyword-face
      (opaque_modifier) @font-lock-keyword-face
      (infix_modifier) @font-lock-keyword-face
      (transparent_modifier) @font-lock-keyword-face
      (open_modifier) @font-lock-keyword-face
      (inline_modifier) @font-lock-keyword-face
-     [,@scala-ts-mode--keywords-control] @font-lock-keyword-face
+     [,@scala-ts--keywords-control] @font-lock-keyword-face
      (null_literal) @font-lock-constant-face
      (wildcard) @font-lock-builtin-face
      (annotation) @font-lock-preprocessor-face
@@ -155,8 +131,8 @@
 
    :language 'scala
    :feature 'extra
-   `([,@scala-ts-mode--brackets] @font-lock-bracket-face
-     [,@scala-ts-mode--delimiters] @font-lock-delimiter-face)
+   `([,@scala-ts--brackets] @font-lock-bracket-face
+     [,@scala-ts--delimiters] @font-lock-delimiter-face)
    
    :language 'scala
    :feature 'comment
@@ -287,15 +263,14 @@
       (string)
       (character_literal)
       (interpolated_string)
-      (interpolated_string_expression) ; TODO is this redundant?
+      (interpolated_string_expression)
       ] @font-lock-string-face
      (interpolation) @font-lock-string-face
      (interpolation "$" @font-lock-string-face)))
   "Treesitter font-lock settings for `scala-ts-mode'.")
 
-;; TODO We'll get to this later
-(defun scala-ts-mode--indent-end (node _parent _bol)
-  "Indent NODE if it is an end clause."
+(defun scala-ts--indent-end (node _parent _bol)
+  "Indent end NODE."
   (let ((label (treesit-node-text (treesit-node-next-sibling node) t)))
     (cond
      ;; Check if end <identifier> is already correctly indented
@@ -312,16 +287,6 @@
               (treesit-node-text
                (treesit-node-child prev-sibling 0 t)))
              (treesit-node-start prev-sibling))))
-     ;; check if end val is already correctly indented
-     ((let ((next-sibling (treesit-node-next-sibling node))
-            (prev-sibling (treesit-node-prev-sibling node)))
-        (and (string=
-              (treesit-node-type next-sibling)
-              "val")
-             (string=
-              "val_definition"
-              (treesit-node-type prev-sibling))
-             (treesit-node-start prev-sibling))))
      ;; indent end <identifier> correctly
      ((string= (treesit-node-type (treesit-node-next-sibling node))
                "_end_ident")
@@ -336,35 +301,16 @@
                                             (treesit-node-child node 0 t))))))))
      ;; indent end <everything eles> correctly
      (t
-      (treesit-node-start (treesit-parent-until
-                           node
-                           (lambda (node)
-                             (pcase label
-                               ("if" (scala-ts-mode--node-type=
-                                      "if_expression" node))
-                               ("while" (scala-ts-mode--node-type=
-                                         "while_expression" node))
-                               ("for" (scala-ts-mode--node-type=
-                                       "for_expression" node))
-                               ("match" (scala-ts-mode--node-type=
-                                         "match_expression" node))
-                               ("try" (scala-ts-mode--node-type=
-                                       "try_expression" node))
-                               ("new" (scala-ts-mode--node-type=
-                                       "instance_expression" node))
-                               ("given" (scala-ts-mode--node-type=
-                                         "given_expression" node))
-                               ("val" (scala-ts-mode--node-type=
-                                       "val_definition" node))))))))))
+      (save-excursion
+        (goto-char (treesit-node-start node))
+        (re-search-backward label)
+        (back-to-indentation)
+        (point))))))
 
 
-(defun scala-ts-mode--node-type= (type node)
-  "Compare TYPE and type of NODE for string equality."
-  (string= type (treesit-node-type node)))
-
-(defun scala-ts-mode--indent-if (node parent _bol)
-  "Indent NODE at BOL if it's PARENT is if_expression."
-  (let ((offset scala-ts-mode-indent-offset))
+(defun scala-ts--indent-if (node parent _bol)
+  "Indent NODE with if_expression PARENT."
+  (let ((offset scala-ts-indent-offset))
     (cond
      ((and (string= (treesit-node-type node)
                     "else")
@@ -402,61 +348,159 @@
                                        (string= (treesit-node-type cur-node)
                                                 "if_expression")))))))))
 
-(defun scala-ts-mode--indent-no-node (_node _parent bol)
-  "Indent NODE at BOL with PARENT if it matches no-node."
+(defun scala-ts--indent-no-node (_node _parent _bol)
+  "Indent when the node at point is nil."
   (save-excursion
-    (let ((offset scala-ts-mode-indent-offset)
-          (pos (re-search-backward "[=:]$" nil t)))
-      (if pos
-          (progn
-            (goto-char pos)
-            (back-to-indentation)
-            (+ offset (point)))
-        bol))))
+    (let* ((offset scala-ts-indent-offset)
+           (pos (re-search-backward
+                 (rx (not
+                      (in control
+                          space
+                          blank)))))
+           (last-node (treesit-node-at pos)))
+      (pcase (treesit-node-text last-node t)
+        ((rx (| "for"
+                "yield"
+                "if"
+                "then"
+                "else"
+                "try"
+                "catch"
+                "finally")
+             eol)
+         (goto-char (treesit-node-start last-node))
+         (backward-word)
+         (if (looking-at-p "end")
+             (point)
+           (goto-char (treesit-node-start last-node))
+           (+ offset (point))))
+        
+        ((rx (| ":"
+                "="
+                "{"
+                "with")
+             eol)
+         (re-search-backward (rx (| ":"
+                                    "="
+                                    "{")
+                                 eol)
+                             (treesit-node-start last-node)
+                             t)
+         ;; (goto-char (treesit-node-end last-node))
+         (back-to-indentation)
+         (+ offset (point)))
 
-(defvar scala-ts-mode--indent-rules
-  (let ((offset scala-ts-mode-indent-offset))
+        ("match"
+         (goto-char (treesit-node-start last-node))
+         (backward-word)
+         (if (looking-at-p "end")
+             (point)
+           (+ offset (point))))
+        
+        ((rx "}" eol)
+         (goto-char (treesit-node-start last-node))
+         (back-to-indentation)
+         (if (= (point)
+                (treesit-node-start last-node))
+             (treesit-node-start last-node)
+           (- (point) offset))) ; essentially a dedent
+        
+        (_
+         (goto-char (treesit-node-end last-node))
+         (back-to-indentation)
+         (point))))))
+
+;; TODO fix and clean up
+(defun scala-ts--indent-for (node parent _bol)
+  "Indent NODE if it's PARENT is if_expression."
+  (if (string= (treesit-node-type node)
+               "ERROR")
+      (treesit-node-start (treesit-node-parent parent))
+    (let ((offset scala-ts-indent-offset)
+          (prev-sibling (treesit-node-prev-sibling node)))
+      (if prev-sibling
+          (treesit-node-start prev-sibling)
+        (+ offset (treesit-node-start (treesit-node-parent parent)))))))
+
+(defvar scala-ts--indent-rules
+  (let ((offset scala-ts-indent-offset))
     `((scala
-       ((node-is "comment") no-indent 0)
-       ((node-is "}") parent-bol 0)
-       ;; ((node-is "end") scala-ts-mode--indent-end 0)
-       ((parent-is "if_expression") scala-ts-mode--indent-if 0)
-       ((parent-is ,(rx-to-string
-                     '(or "trait_definition"
-                          "function_definition"
-                          "object_definition"
-                          "class_definition"
-                          "enum_definition"
-                          "val_definition"
-                          "var_definition"
-                          "enum_body"
-                          "template_body")
-                     t))
-        parent-bol ,offset)
-       ((node-is ,(rx-to-string
-                   '(or "trait_definition"
-                        "function_definition"
-                        "object_definition"
-                        "class_definition"
-                        "enum_definition"
-                        "val_definition"
-                        "var_definition"
-                        "enum_body"
-                        "template_body")
-                   t))
-        prev-sibling 0)
-       ((node-is "indented_block") parent-bol ,offset)
-       ((node-is "block") parent ,offset)
-       ((parent-is "indented_block") parent 0)
-       ((query ((ERROR) @return)) no-indent 0)
-       (no-node
-        scala-ts-mode--indent-no-node
-        0))))
+       ((node-is "^comment$") no-indent 0)
+       
+       ((node-is "^}$") parent-bol 0)
+       
+       ((parent-is "^if_expression$") scala-ts--indent-if 0)
+       
+       ((n-p-gp "^enumerators$" "^for_expression$" nil) parent ,offset)
+       ((n-p-gp "^ERROR$" "^enumerators$" nil) prev-line ,(- offset))
+       ((n-p-gp "^yield$" "^for_expression$" nil) parent 0)
+       
+       ((n-p-gp "^catch$" "^try_expression$" nil) parent 0)
+       
+       ((node-is "^end$") scala-ts--indent-end 0)
+       
+       ;; Handle function annotations
+       ((n-p-gp "^def$" "^function_definition$" nil) parent 0)
+
+       
+       ((parent-is "^ERROR$")
+        (lambda (n p b)
+          (message "Parent: %s" (treesit-node-text (treesit-node-child p 0)))
+          b)
+        0)
+       
+       ;; The beast, no-nodes (and ERROR nodes)
+       (no-node scala-ts--indent-no-node 0)
+       
+       ((parent-is "^compilation_unit$") column-0 0)
+       ((parent-is "^trait_definition$") parent-bol ,offset)
+       ((parent-is "^function_definition$") parent-bol ,offset)
+       ((parent-is "^object_definition$") parent-bol ,offset)
+       ((parent-is "^class_definition$") parent-bol ,offset)
+       ((parent-is "^enum_definition$") parent-bol ,offset)
+       ((parent-is "^val_definition$") parent-bol ,offset)
+       ((parent-is "^var_definition$") parent-bol ,offset)
+       ((parent-is "^enum_body$") parent-bol ,offset)
+       ((parent-is "^template_body$") parent-bol ,offset)
+       
+       ((node-is "^trait_definition$") prev-sibling 0)
+       ((node-is "^function_definition$") prev-sibling 0)
+       ((node-is "^object_definition$") prev-sibling 0)
+       ((node-is "^class_definition$") prev-sibling 0)
+       ((node-is "^enum_definition$") prev-sibling 0)
+       ((node-is "^val_definition$") prev-sibling 0)
+       ((node-is "^var_definition$") prev-sibling 0)
+       ((node-is "^enum_body$") prev-sibling 0)
+       ((node-is "^template_body$") prev-sibling 0)
+
+       ((n-p-gp "^identifier$" "^indented_block$" nil)
+        (lambda (n p _bol)
+          (when (member (treesit-node-text n)
+                        '("then" "catch" "else" "finally"))
+            (- (treesit-node-start p) ,offset)))
+        0)
+       
+       ((n-p-gp "^call_expression$" "^indented_block$" nil)
+        (lambda (n p _bol)
+          (when (member (treesit-node-text (treesit-node-child-by-field-name
+                                            n
+                                            "function"))
+                        '("then" "catch" "else" "finally"))
+            (- (treesit-node-start p) ,offset)))
+        0)
+       
+       ((n-p-gp "^indented_block$" "^ERROR$" nil) no-indent 0)
+       ((parent-is "^indented_block$") parent 0)
+       ((node-is "^indented_block$") parent-bol ,offset)
+       ((node-is "^block$") parent ,offset)
+       
+       ;; Uhhh I don't think this is very good :)
+       )))
   "Tree-sitter indent rules for `scala-ts-mode'.")
 
-(defun scala-ts-mode--defun-name (node)
+(defun scala-ts--defun-name (node)
   "Return the defun name of NODE.
-Return nil if there is no nameor if NODE is not a defun node."
+Return nil if there is no name or if NODE is not a defun node."
   (pcase (treesit-node-type node)
     ((or "class_definition"
          "trait_definition"
@@ -467,13 +511,17 @@ Return nil if there is no nameor if NODE is not a defun node."
          "function_definition")
      (treesit-node-text
       (treesit-node-child-by-field-name node "name")
+      t))
+    ("function_declaration"
+     (treesit-node-text
+      (treesit-node-child node 0)
       t))))
 
 ;;;###autoload
 (define-derived-mode scala-ts-mode prog-mode " Scala (TS)"
   "Major mode for Scala files using tree-sitter."
   :group 'scala-ts
-  :syntax-table scala-ts-mode--syntax-table
+  :syntax-table scala-ts--syntax-table
 
   (when (treesit-ready-p 'scala)
     (treesit-parser-create 'scala)
@@ -483,7 +531,7 @@ Return nil if there is no nameor if NODE is not a defun node."
     (setq-local comment-end "")
     (setq-local comment-start-skip (rx "//" (* (syntax whitespace))))
 
-    (setq-local treesit-font-lock-settings scala-ts-mode--treesit-font-lock-settings)
+    (setq-local treesit-font-lock-settings scala-ts--treesit-font-lock-settings)
     ;; TODO Split this into levels to respect user choices
     (setq-local treesit-font-lock-feature-list '((comment doc-comment definition)
                                                  (keyword  type)
@@ -492,16 +540,17 @@ Return nil if there is no nameor if NODE is not a defun node."
 
 
     (setq-local
-     treesit-simple-indent-rules scala-ts-mode--indent-rules)
+     treesit-simple-indent-rules scala-ts--indent-rules)
 
-    (setq-local treesit-defun-name-function #'scala-ts-mode--defun-name)
+    (setq-local treesit-defun-name-function #'scala-ts--defun-name)
     ;; TODO (could possibly be more complex?)
     (setq-local treesit-simple-imenu-settings
                 `(("Class" "\\`class_definition\\'" nil nil)
                   ("Trait" "\\`trait_definition\\'" nil nil)
                   ("Enum" "\\`enum_definition\\'"' nil nil)
                   ("Object" "\\`object_definition\\'" nil nil)
-                  ("Function" "\\`function_definition\\'" nil nil)))
+                  ("Function" "\\`function_definition\\'" nil nil)
+                  ("Definition" "\\`function_declaration'" nil nil)))
     
     (treesit-major-mode-setup)))
 
